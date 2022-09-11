7b2601dbfde9d64ccf550ff6eb558e92713eb3e5
RFNshare <rfnshare@gmail.com>
2022-09-11 21:49:55 +0600

    Hello Wrold Added

---
+++ src/lecture_online/hello_world.c
@@ -0,0 +1,18 @@
+// Author: Abdullah Al Faroque
+// Date: 9/11/2022
+/* Program for printing Hello World */
+#include <stdio.h>
+#include <conio.h>
+
+void display();
+
+void main()
+{
+    printf("Hello World\n");
+    display();
+    getch();
+}
+void display()
+{
+    printf("Congratulations, you did it!!!");
+}
