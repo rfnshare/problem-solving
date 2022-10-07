7d3d90031d6fe0f49569192adae7eb4cf906e5af
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-07 11:13:13 +0600

    print all primes smaller than or equal to n

--- src/lecture_online/find_all_prime.c
+++ src/lecture_online/find_all_prime.c
@@ -6,27 +6,22 @@
 #include <math.h>
 
 void main() {
-  int range, c = 0;
-  scanf("%d", & range);
-  int composite[range];
-  int r = sqrt(range);
-  for (int i = 2; i <= r; i++) {
-    for (int j = 2; i * j <= range; j++) {
-      int k = i * j;
-      //composite[c] = k;
-      //printf("Array %d -->%d\n", c, composite[c]);
-      int length = sizeof(composite) / sizeof(composite[0]);
-      for (int l = 0; l <= length; l++) {
-        if (composite[l] == k) {
+    int range;
+    scanf("%d", & range);
+
+    for (int i = 2; i <= range; i++) {
+      int r = sqrt(i);
+      if (i<=3)
+      {
+          printf("%d\n", i);
+      }
+      for (int j = 2; j <= r; j++) {
+        if (i % j == 0) {
           break;
         }
-        if (l == length) {
-          composite[c] = k;
-          printf("Array %d -->%d\n", c, composite[c]);
-        }
+        if (j == r) {
+          printf("%d\n", i);
       }
-      c += 1;
-
     }
 
   }
