e54d6c324e79ab6cfda69de85f6531351a0f0888
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-16 21:31:55 +0600

    Input & Output

---
+++ src/phitron/input.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    // How to take input?
+    int a,b;
+    printf("Enter two number: ");
+    scanf("%d%d", &a, &b);
+
+    // How to show output?
+    printf("The Value of a is %d\n", a);
+    printf("The Value of b is %d", b);
+    return 0;
+}
