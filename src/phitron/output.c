88614c7e4f9ec4fb7ca82cc1becdc78b3d6eb118
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-17 20:25:52 +0600

    Normal Output

---
+++ src/phitron/output.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+// normal output tracing
+int main()
+{
+    printf("Hi there /\\.\tI hope you are doing great and practicing well.\t\nNow this is a simple task you need to do.\tJust care for the indentation.\nShow 100% of sincerity.");
+    return 0;
+}
