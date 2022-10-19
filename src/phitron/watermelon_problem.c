b58cad300df8db47bc5bac83b7ffc987ccc90cd6
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-19 21:25:31 +0600

    watermelon problem

---
+++ src/phitron/watermelon_problem.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<math.h>
+// find out watermelon is even cut check for two person
+int main()
+{
+    int w;
+    printf("Enter watermelon Weight: ");
+    scanf("%d", &w);
+    if(w%2==0 && 2<w)
+    {
+        printf("Possible");
+    }
+    else
+    {
+        printf("Not Possible");
+    }
+
+    return 0;
+}
+
