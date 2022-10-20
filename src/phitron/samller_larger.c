edfcf84fa62e3efb33581c51b8ad98da5320c777
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-20 22:19:45 +0600

    m & n problem

---
+++ src/phitron/samller_larger.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+
+int main()
+{
+    int m;
+    printf("Sample Non Negative Input:\n");
+    scanf("%d", &m);
+    if(0<m)
+    {
+        printf("The value of n = 1");
+    }
+    else if(m==0)
+    {
+        printf("The value of n = 0");
+    }
+    else
+    {
+        printf("The value of n = -1");
+    }
+
+
+
+    return 0;
+}
+
+
