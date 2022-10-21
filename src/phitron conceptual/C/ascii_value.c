cf8720340e7d7deacef2bdec4d9de3e54afdb46b
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-21 17:45:29 +0600

    ascii sub

--- src/phitron conceptual/C/ascii_value.c
+++ src/phitron conceptual/C/ascii_value.c
@@ -2,8 +2,8 @@
 
 int main()
 {
-    char c;
-    scanf("%c", &c);
-    printf("Here ASCII value %d", c);
+    char c,d;
+    scanf("%c %c", &c, &d);
+    printf("Here ASCII value %d %d & Sub is %d", c, d, c-d);
     return 0;
 }
