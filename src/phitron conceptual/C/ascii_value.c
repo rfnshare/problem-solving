65b5394e29cda3e33a45fdd7d5acfeda365730dc
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-21 17:49:11 +0600

    int to char in ascii

--- src/phitron conceptual/C/ascii_value.c
+++ src/phitron conceptual/C/ascii_value.c
@@ -4,6 +4,9 @@ int main()
 {
     char c,d;
     scanf("%c %c", &c, &d);
-    printf("Here ASCII value %d %d & Sub is %d", c, d, c-d);
+    printf("Here ASCII value %d %d & Sub is %d\n", c, d, c-d);
+    int a;
+    scanf("%d", &a);
+    printf("%c", a);
     return 0;
 }
