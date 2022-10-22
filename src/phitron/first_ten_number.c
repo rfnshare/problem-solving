73dd38ce0c2ee53ed430f6d3d1428995b113b1a3
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-22 20:37:51 +0600

    display n terms of natural number and their sum

--- src/phitron/first_ten_number.c
+++ src/phitron/first_ten_number.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main()
 {
-    int a;
+    int a,c=0;
     scanf("%d", &a);
     for(int i=1; i<=a; i++)
     {
-        printf("%d ",i);
+        c += i;
     }
+    printf("The Sum of Natural Number upto %d terms :%d",a,c);
     
 }
