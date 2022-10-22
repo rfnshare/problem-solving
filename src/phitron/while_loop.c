885470f1c247216b25f252f4be570bc0027e0605
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-22 21:32:14 +0600

    while loop with sum

--- src/phitron/while_loop.c
+++ src/phitron/while_loop.c
@@ -2,12 +2,14 @@
 
 int main()
 {
-    int i = 1;
-    while (i<=10)
+    int i = 1, sum = 0;
+    while (i <=5)
     {
         printf("%d\n", i);
+        sum += i;
         i++;
     }
-    
+    printf("Sum %d",sum);
+
     return 0;
 }
