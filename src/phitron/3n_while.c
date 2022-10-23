cd4aed976d579c303055d614ca3537a05cc1e42d
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-23 11:35:39 +0600

    The Collatz Sequence Problem

---
+++ src/phitron/3n_while.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (1<n)
+    {
+        if(n%2==0)
+        {
+            n = n/2;
+        }
+        else
+        {
+            n = 3*n+1;
+        }
+        printf("%d\n", n);
+    }
+    
+}
