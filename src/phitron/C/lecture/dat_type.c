40f1092b3938820520c41a03c279fdc71c9521a2
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-28 18:19:48 +0600

    data type size checking

---
+++ src/phitron/C/lecture/dat_type.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    printf("%d\n", sizeof(int));
+    printf("%d\n", sizeof(long int));
+    printf("%d\n", sizeof(long long int));
+    printf("%d\n", sizeof(float));
+    printf("%d\n", sizeof(double));
+    printf("%d\n", sizeof(char));
+}
