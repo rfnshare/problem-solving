d6c1655218c250ca5ffe8529f9bf5a7c5321e575
Abdullah Al Faroque <rfnshare@gmail.com>
2022-10-30 08:09:53 +0600

    array with looping

--- src/phitron/C/lecture/array.c
+++ src/phitron/C/lecture/array.c
@@ -2,8 +2,16 @@
 int main()
 {
     int arr[20];
-    arr[6] = 75;
-    arr[0] = 29;
-    arr[2] = arr[6] + arr[0];
-    printf("%d", arr[2]);
+    // arr[6] = 75;
+    // arr[0] = 29;
+    // arr[2] = arr[6] + arr[0];
+    // printf("%d", arr[2]);
+    for (int i = 0; i < 5; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d-th position value = %d\n", i, arr[i]);
+    }
 }
