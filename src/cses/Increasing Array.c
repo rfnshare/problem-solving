14bdc8f297a8272b31623211eb14573c2c13210f
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-01 21:16:11 +0600

    Increasing Array CSES

---
+++ src/cses/Increasing Array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+int main()
+{
+    int N, x, i, k, count = 0;
+    int arr[300000];
+    scanf("%d", &N);
+
+    for (int i = 1; i <= N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    for (int j = 1; j < N; j++)
+    {
+        if (arr[j] < arr[j + 1])
+        {
+            continue;
+        }
+        else
+        {
+            while (arr[j + 1] < arr[j])
+            {
+                arr[j + 1] = arr[j + 1] + 1;
+                count++;
+            }
+        }
+    }
+    printf("%d", count);
+}
