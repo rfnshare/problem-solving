2b40d871d2aec2998ecf60da365dc16e4a63620d
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-02 20:56:47 +0600

    prob 1 updated

--- src/phitron/C/practice/W3P2/prob 1.c
+++ src/phitron/C/practice/W3P2/prob 1.c
@@ -1,15 +1,26 @@
 #include <stdio.h>
 int main()
 {
-    int i, j, N, a = 0, b = 1, c ,sum;
+    int i, j, N, a = 0, b = 1, c, sum;
     scanf("%d", &N);
-    printf("%d %d ", a, b);
-    for (i = 0; i < N - 2; i++)
+    if (N == 1)
     {
-        sum = a + b; // 1
-        printf("%d ", sum);
-        c = b;
-        b = sum;
-        a = c;
+        printf("%d ", a);
+    }
+    else if (N == 2)
+    {
+        printf("%d %d ", a, b);
+    }
+    else
+    {
+        printf("%d %d ", a, b);
+        for (i = 0; i < N - 2; i++)
+        {
+            sum = a + b; // 1
+            printf("%d ", sum);
+            c = b;
+            b = sum;
+            a = c;
+        }
     }
 }
