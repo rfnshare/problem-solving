60421c1bf560c413b1cc66cf0d6262817e2c0d69
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-02 22:10:23 +0600

    prob 4 half solved

---
+++ src/phitron/C/practice/W3P2/prob 4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i, j, N, arr[100],pos;
+    scanf("%d", &N);
+    for (i = 0; i < N; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &pos);
+}
