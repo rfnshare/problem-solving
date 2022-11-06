554efa7ca2f512a157b3c9c3a790e4eb113dc8c6
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-06 20:25:41 +0600

    word capitalization

---
+++ src/codeforces/word capitalization.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int main()
+{
+    char st[1000];
+    int count = 0;
+    fgets(st, sizeof(st), stdin);
+    for (int i = 0; i<1; i++)
+    {
+        if (st[i] > 'a' && st[i] < 'z')
+        {
+            st[i] -= 32;
+        }
+    }
+    puts(st);
+    return 0;
+}
