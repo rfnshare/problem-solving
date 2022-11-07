a9c3686e320d6275c209afb559def458e6047163
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-07 14:40:53 +0600

    diagonal matrix

---
+++ src/phitron/C/lecture/diagonal matrix.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+int main()
+{
+    int row, column, ans;
+    scanf("%d%d", &row, &column);
+    int a[row][column], i, j, count = 0;
+
+    for (i = 0; i < row; i++)
+    {
+        for (j = 0; j < column; j++)
+        {
+            scanf("%d", &a[i][j]);
+        }
+    }
+    if (row == column)
+    {
+        ans = 1;
+        for (i = 0; i < row; i++)
+        {
+            for (j = 0; j < column; j++)
+            {
+                if (i == j)
+                {
+                    continue;
+                }
+
+                else if (a[i][j] != 0)
+                {
+                    ans = 0;
+                }
+            }
+            // printf("\n");
+        }
+    }
+    else
+    {
+        ans = 0;
+    }
+    if (ans == 1)
+    {
+        printf("Diagonal\n");
+    }
+    else
+    {
+        printf("Not Diagonal\n");
+    }
+}
