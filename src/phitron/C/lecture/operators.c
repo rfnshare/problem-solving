d43bdf87299ec1d5d5e43f782337031fe242464d
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-13 20:43:58 +0600

    char with operators

--- src/phitron/C/lecture/operators.c
+++ src/phitron/C/lecture/operators.c
@@ -2,23 +2,30 @@
 #include <math.h>
 int main()
 {
-    int a, b, c, d, m, q, r;
-    a = 7;
-    b = 5;
-    c = a + b; // addition
-    d = a - b; // subtract
-    m = a * b; // multiply
-    q = a / b; // divide
-    r = a % b; // reminder
-    printf("Addition = %d\n", c);
-    printf("Subtract = %d\n", d);
-    printf("Multiply = %d\n", m);
-    printf("Divide = %d\n", q);
-    printf("Reminder = %d\n", r);
-    int p = pow(a, b);
-    printf("power = %d\n", p);
-    float s ;
-    s = sqrt(48);
-    printf("sqrt = %0.2f\n", s);
+    // int a, b, c, d, m, q, r;
+    // a = 7;
+    // b = 5;
+    // c = a + b; // addition
+    // d = a - b; // subtract
+    // m = a * b; // multiply
+    // q = a / b; // divide
+    // r = a % b; // reminder
+    // printf("Addition = %d\n", c);
+    // printf("Subtract = %d\n", d);
+    // printf("Multiply = %d\n", m);
+    // printf("Divide = %d\n", q);
+    // printf("Reminder = %d\n", r);
+    // int p = pow(a, b);
+    // printf("power = %d\n", p);
+    // float s;
+    // s = sqrt(48);
+    // printf("sqrt = %0.2f\n", s);
+
+    char a, b;
+    a = 'x';
+    b = 's';
+    char c;
+    c = a + 2;
+    putchar(c);
     return 0;
 }
