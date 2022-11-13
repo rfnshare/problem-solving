1e6099a2c171cdb988510355c361a4f99fe297a9
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-13 20:33:23 +0600

    basic operators

---
+++ src/phitron/C/lecture/operators.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a, b, c, d, m, q, r;
+    a = 7;
+    b = 5;
+    c = a + b; // addition
+    d = a - b; // subtract
+    m = a * b; // multiply
+    q = a / b; // divide
+    r = a % b; // reminder
+    printf("Addition = %d\n", c);
+    printf("Subtract = %d\n", d);
+    printf("Multiply = %d\n", m);
+    printf("Divide = %d\n", q);
+    printf("Reminder = %d\n", r);
+    int p = pow(a, b);
+    printf("power = %d\n", p);
+    float s ;
+    s = sqrt(48);
+    printf("sqrt = %0.2f\n", s);
+    return 0;
+}
