6adf48f6f9aeb704764dbadce0b2f252d4bf1a01
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-13 13:19:49 +0600

    using getchar function for char input

--- src/phitron/C/lecture/recap_input.c
+++ src/phitron/C/lecture/recap_input.c
@@ -13,9 +13,12 @@ int main()
     // scanf("%d", &a);
     // scanf("%f", &b);
     //scanf("%d,%f", &a, &b);
-    scanf("%c ", &ch);
-    scanf("%c", &ch2);
+    // scanf("%c ", &ch);
+    // scanf("%c", &ch2);
     // printf("%d %f", a, b);
-    printf("%c %c", ch, ch2);
+    ch = getchar();
+    getchar();
+    ch2 = getchar();
+    printf("%c %d", ch, ch2);
     return 0;
 }
