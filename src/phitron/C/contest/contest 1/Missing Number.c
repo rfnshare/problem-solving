b34c006290d2cdc6c38b553c5d50e40dfa275d07
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-13 19:30:21 +0600

    missing number updated

--- src/phitron/C/contest/contest 1/Missing Number.c
+++ src/phitron/C/contest/contest 1/Missing Number.c
@@ -2,15 +2,14 @@
 int main()
 {
     int T;
-    unsigned long long int S, A, B, C, r;
+    long long int S, A, B, C, r;
     scanf("%d", &T);
-    if (0 < T && T <= 100)
+
+    for (int i = 0; i < T; i++)
     {
-        for (int i = 0; i < T; i++)
-        {
-            scanf("%lu%lu%lu%lu", &S, &A, &B, &C);
-        }
+        scanf("%lld%lld%lld%lld", &S, &A, &B, &C);
         r = S - (A + B + C);
-        printf("%lu\n", r);
+        printf("%lld\n", r);
+        r = 0;
     }
 }
