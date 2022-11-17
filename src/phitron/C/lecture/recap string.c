c28859789decfd8e66842aace57edf78cfa9982b
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-17 09:33:32 +0600

    recap string

---
+++ src/phitron/C/lecture/recap string.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char name[] = "Abdullah Al";
+    char name2[] = " Faroque";
+    strcat(name,name2);
+    puts(name);
+    
+    return 0;
+    
+}
