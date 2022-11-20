50e2b32f4572649bb1a6c5f579cabeb54f9c9cb8
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-20 14:36:26 +0600

    Cards Of Friends

--- src/codeforces/Cards for Friends.c
+++ src/codeforces/Cards for Friends.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 int main()
 {
-    int t, w, h, n, i, j, k, count = 0;
+    int t, w, h, n, i, j, k, count = 1;
     scanf("%d", &t);
     for (i = 0; i < t; i++)
     {
@@ -15,16 +15,15 @@ int main()
             while (w % 2 == 0)
             {
                 w /= 2;
-                count += 2;
+                count *= 2;
             }
             while (h % 2 == 0)
             {
                 h /= 2;
-                count += 2;
+                count *= 2;
             }
         }
-
-        // printf("%d", count);
+        // printf("Count = %d\n", count);
         if (count >= n)
         {
             printf("YES\n");
@@ -33,6 +32,6 @@ int main()
         {
             printf("NO\n");
         }
-        count = 0;
+        count = 1;
     }
 }
