92e9fac0a9c9fb97e6d9b695c9e5d150a03340a6
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-20 11:02:23 +0600

    Cards for friend, not done

---
+++ src/codeforces/Cards for Friends.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int main()
+{
+    int t, w, h, n, i, j, k, count = 0;
+    scanf("%d", &t);
+    for (i = 0; i < t; i++)
+    {
+        scanf("%d%d%d", &w, &h, &n);
+        if (w % 2 != 0 && h % 2 != 0)
+        {
+            count = 1;
+        }
+        else
+        {
+            while (w % 2 == 0)
+            {
+                w /= 2;
+                count += 2;
+            }
+            while (h % 2 == 0)
+            {
+                h /= 2;
+                count += 2;
+            }
+        }
+
+        // printf("%d", count);
+        if (count >= n)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+        count = 0;
+    }
+}
