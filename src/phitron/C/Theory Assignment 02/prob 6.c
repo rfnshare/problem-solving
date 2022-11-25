86828953c596f2652a79ea2e0b07d5c00f9f5580
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-25 14:08:17 +0600

    prob 6 updated

--- src/phitron/C/Theory Assignment 02/prob 6.c
+++ src/phitron/C/Theory Assignment 02/prob 6.c
@@ -3,10 +3,13 @@ int factorial(int x);
 float ratio(int x, int y);
 int main()
 {
-    int a, b, j, k;
+    int a, b, j, k, f;
+    scanf("%d", &f);
     scanf("%d%d", &a, &b);
+    int fact = factorial(f);
+    printf("%d\n", fact);
     float i = ratio(a, b);
-    printf("%f", i);
+    printf("%.2f", i);
 }
 int factorial(int x)
 {
