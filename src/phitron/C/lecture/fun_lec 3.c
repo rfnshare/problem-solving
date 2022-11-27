800d2c8aded7d1ebb0f9b00d0ecc5aebc37f0d70
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-27 11:52:50 +0600

    function recap in c

---
+++ src/phitron/C/lecture/fun_lec 3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void solve()
+{
+    printf("WARNING!!!");
+}
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 0; i < t; i++)
+    {
+        solve();
+    }
+    warning();
+    return 0;
+}
