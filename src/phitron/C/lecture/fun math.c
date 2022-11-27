4f2c149a1ecb1780f814e0b3c227899037e6ea4f
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-27 14:30:48 +0600

    different functions in math

---
+++ src/phitron/C/lecture/fun math.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int a, b, i, j, k;
+    a = 3, b = -2;
+    float ans;
+    ans = cos(3.141593/2);
+    printf("%f\n", pow(a, b));
+    printf("%f\n", sqrt(a));
+    printf("%d\n", abs(b));
+    printf("%f\n", ans);
+}
