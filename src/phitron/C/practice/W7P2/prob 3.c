a2cce255cff7a5f0361c28b563649d6a67fa960b
aalfaroque <rfnshare@gmail.com>
2022-11-30 22:59:46 +0600

    problem 3 half

---
+++ src/phitron/C/practice/W7P2/prob 3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int arr_print(int arr[], n);
+int main()
+{
+    int arr[100],i,n;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    arr_print(arr,n);
+    
+}
+int arr_print(int arr[], n)
+{
+    for(i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+}
