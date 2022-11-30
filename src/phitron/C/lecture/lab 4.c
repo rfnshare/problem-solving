0334f788db77a6964aa8caa75df4e5a296cc1257
Abdullah Al Faroque <rfnshare@gmail.com>
2022-11-30 20:14:51 +0600

    recursioin adding numbers

---
+++ src/phitron/C/lecture/lab 4.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int recursion(int a);
+int main()
+{
+    int a, b, i, j, k;
+    scanf("%d", &a);
+    b = recursion(a);
+    printf("%d", b);
+}
+int recursion(int a)
+{
+    if (0 < a)
+    {
+        return recursion(a - 1) + a;
+    }
+}
