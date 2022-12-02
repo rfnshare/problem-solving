a4ff6a7b85df26c02a7311b21e5743b222ca1c03
Abdullah Al Faroque <rfnshare@gmail.com>
2022-12-03 02:08:28 +0600

    swap

---
+++ src/phitron/C/Lab Assigment 2/swap.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+int swap(int *a, int *b);
+int main()
+{
+    int n, i, arr[100], q, j, arr1[100], arr2[100], arr3[100], count = 0;
+    scanf("%d", &n);
+    for (i = 1; i <= n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    scanf("%d", &q);
+    for (i = 1; i <= q; i++)
+    {
+        scanf("%d", &arr1[i]);
+        scanf("%d", &arr2[i]);
+        swap(&arr[arr1[i]], &arr[arr2[i]]);
+        // arr3[count] = arr1[i];
+        // arr3[count + 1] = arr2[i];
+        // count += 2;
+    }
+    for (i = 1; i <= n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+int swap(int *a, int *b)
+{
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
