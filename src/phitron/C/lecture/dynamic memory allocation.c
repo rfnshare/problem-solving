f85df37393c2764e2db0d2d43e00e655e1210435
Abdullah Al Faroque <rfnshare@gmail.com>
2022-12-04 00:04:38 +0600

    without initilize array by dynamic memory

---
+++ src/phitron/C/lecture/dynamic memory allocation.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main()
+{
+    int n, i;
+    scanf("%d", &n);
+    int *ptr;
+
+    ptr = (int)malloc(n * sizeof(int));
+
+    for (i = 0; i < n; i++)
+    {
+        scanf("%d", (ptr + i));
+    }
+    for (i = 0; i < n; i++)
+    {
+        printf("%d ", *(ptr + i));
+    }
+}
