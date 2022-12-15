b42800e9de6ee51cfd63bf678a6bdc45070802b6
Abdullah Al Faroque <rfnshare@gmail.com>
2022-12-15 11:31:00 +0600

    complete tic tac toe game

--- src/phitron/C/game tik tae toe/tic - tac - toe.c
+++ src/phitron/C/game tik tae toe/tic - tac - toe.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
 void print_grid(int arr[][4]);
+int result(int arr[][4]);
 int main()
 {
     int a, b, i, j, k;
@@ -47,8 +48,53 @@ int main()
             p2 = false;
             p1 = true;
         }
+        if (result(arr) == 1)
+        {
+            printf("Player 1 Won\n");
+            print_grid(arr);
+            break;
+        }
+        else if (result(arr) == 2)
+        {
+            printf("Player 2 Won\n");
+            print_grid(arr);
+            break;
+        }
+        // else
+        // {
+        //     printf("Draw\n");
+        //     break;
+        // }
     }
     return 0;
+}
+int result(int arr[][4])
+{
+    int i, j, k;
+    for (i = 1; i <= 3; i++)
+    {
+        if (arr[i][1] == arr[i][2] && arr[i][2] == arr[i][3] && arr[i][1] != -1)
+        {
+            return arr[i][1];
+        }
+    }
+    for (j = 1; j <= 3; j++)
+    {
+        if (arr[1][j] == arr[2][j] && arr[2][j] == arr[3][j] && arr[1][j] != -1)
+        {
+            return arr[1][j];
+        }
+    }
+    if (arr[1][1] == arr[2][2] && arr[2][2] == arr[3][3] && arr[1][1] != -1)
+    {
+        return arr[1][1];
+    }
+    if (arr[1][3] == arr[2][2] && arr[2][2] == arr[3][1] && arr[1][3] != -1)
+    {
+        return arr[1][3];
+    }
+
+    return -1;
 }
 void print_grid(int arr[][4])
 {
