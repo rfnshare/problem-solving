7763cf54967ff909ee5a9e44e8e8d6ed529bab0f
aalfaroque <rfnshare@gmail.com>
2022-12-25 14:04:32 +0600

    string input using getline with ignore function

--- src/phitron/C++/Lecture/Week 1(Basic Of C++)/main.cpp
+++ src/phitron/C++/Lecture/Week 1(Basic Of C++)/main.cpp
@@ -2,16 +2,23 @@
 using namespace std;
 int main()
 {
-    cout<<"Hello World\n"<<endl;
+    cout<<"Hello World"<<endl;
     int x,y;
-    char ch;
+    char ch,ch2;
     float f;
     double d;
     string s;
-    // cin>>x>>ch>>f>>d;
-    // cout<<x<<" "<<ch<<" "<<f<<" "<<d<<endl;
+    cin>>x>>ch>>f>>d;
+    cout<<x<<" "<<ch<<" "<<f<<" "<<d<<endl;
     // cin >>s;
+    cin.ignore(); //ref https://stackoverflow.com/questions/7786994/c-getline-isnt-waiting-for-input-from-console-when-called-multiple-times
     getline(cin, s);
     cout<<s<<endl;
+    // cin>>ch;
+    // cin>>ch2;
+    // cin.ignore();
+    // getline(cin, s);
+    // cout<<ch<<ch2<<endl;
+    // cout<<s;
     return 0;
 }
