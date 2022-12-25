f183f1d080bfb720d1caafddebb83f3df76f0700
aalfaroque <rfnshare@gmail.com>
2022-12-25 14:22:51 +0600

    input from file and print in console

--- src/phitron/C++/Lecture/Week 1(Basic Of C++)/stringOutputFIles.cpp
+++ src/phitron/C++/Lecture/Week 1(Basic Of C++)/stringOutputFIles.cpp
@@ -3,15 +3,19 @@
 using namespace std;
 int main()
 {
-    string s;
-    ofstream of, of2;
-    of.open("1.txt");
-    of2.open("2.txt");
+    // string s;
+    // ofstream of, of2;
+    // of.open("1.txt");
+    // of2.open("2.txt");
 
     ifstream in1, in2;
     in1.open("input.txt");
-    
-    of<<"Hello"<<endl;
-    of2<<"Hello Two"<<endl;
+    int x;
+    double y,z;
+    in1>>x>>y>>z;
+    cout<<x<<" "<<y<<" "<<z;
+
+    // of<<"Hello"<<endl;
+    // of2<<"Hello Two"<<endl;
     return 0;
 }
