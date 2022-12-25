801ba88f401b8c8912b4752f9a9f73103edeed49
aalfaroque <rfnshare@gmail.com>
2022-12-25 20:57:39 +0600

    pass by reference(swapping)

--- src/phitron/C++/Lecture/Week 1(Basic Of C++)/function.cpp
+++ src/phitron/C++/Lecture/Week 1(Basic Of C++)/function.cpp
@@ -11,6 +11,13 @@ void fun2(string &s) // pass by reference
     s.erase(s.begin());
     s.erase(s.begin() + s.size() - 1);
 }
+void swap(int &a, int &b) // pass by reference
+{
+    int temp;
+    temp = a;
+    a = b;
+    b = temp;
+}
 int main()
 {
     string s;
@@ -19,6 +26,9 @@ int main()
     // cout << fun1(s) << endl;
     fun2(s);
     cout << s << endl;
+    int a = 5, b = 10;
+    swap(a, b);
+    cout << a << " " << b << endl;
 
     return 0;
 }
