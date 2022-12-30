9b5245e332557e0689281b07866a682235688e58
Abdullah Al Faroque <rfnshare@gmail.com>
2022-12-30 12:19:31 +0600

    class & object(multi parenting)

--- src/phitron/C++/Week 1(Basic Of C++)/Lab/class_and_object.cpp
+++ src/phitron/C++/Week 1(Basic Of C++)/Lab/class_and_object.cpp
@@ -14,23 +14,50 @@ public:
     }
 };
 
+class Rectangle
+{
+public:
+    int width, height;
+    Student s;
+    int calc_area()
+    {
+        return width * height;
+    }
+    int calc_perimeter()
+    {
+        return 2 * (height + width);
+    }
+};
+
 int main()
 {
-    cout << "Testing" << endl;
-    Student s;
-    s.name = "Abdullah Al Faroque";
-    s.student_id = 30770;
-    s.age = 26;
-    s.f_name = "Delwar Hossain";
-    s.m_name = "Farida Akter";
-    s.print_info();
+    Rectangle r1;
+    r1.height = 4;
+    r1.width = 8;
+    cout << r1.calc_area() << endl;
+    cout << r1.calc_perimeter() << endl;
+    Rectangle r2;
+    r2.height = 3;
+    r2.width = 9;
+    cout << r2.calc_area() << endl;
+    cout << r2.calc_perimeter() << endl;
+    r1.s.f_name = "Father Name";
+    r1.s.print_info();
+    // cout << "Testing" << endl;
+    // Student s;
+    // s.name = "Abdullah Al Faroque";
+    // s.student_id = 30770;
+    // s.age = 26;
+    // s.f_name = "Delwar Hossain";
+    // s.m_name = "Farida Akter";
+    // s.print_info();
 
-    Student s2;
-    s.name = "Abdullah Al Rashed";
-    s.student_id = 30771;
-    s.age = 23;
-    s.f_name = "Delwar Hossain";
-    s.m_name = "Farida Akter";
-    s.print_info();
+    // Student s2;
+    // s.name = "Abdullah Al Rashed";
+    // s.student_id = 30771;
+    // s.age = 23;
+    // s.f_name = "Delwar Hossain";
+    // s.m_name = "Farida Akter";
+    // s.print_info();
     return 0;
 }
