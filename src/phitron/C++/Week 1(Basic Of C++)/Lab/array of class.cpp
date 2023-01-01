e8f74dfc1a1a7e60da4f17d8237b86fd1bfdcc36
Abdullah Al Faroque <rfnshare@gmail.com>
2023-01-01 20:24:02 +0600

    class array sorting

--- src/phitron/C++/Week 1(Basic Of C++)/Lab/array of class.cpp
+++ src/phitron/C++/Week 1(Basic Of C++)/Lab/array of class.cpp
@@ -19,19 +19,44 @@ public:
     {
         cout << name << " " << student_id << " " << age << " " << f_name << " " << m_name << endl;
     }
+    // operator overloading
+    bool operator<(Student s)
+    {
+        return student_id < s.student_id;
+    }
 };
-
+// bool comp(Student a, Student b)
+// {
+//     //
+//     return a.student_id < b.student_id;
+// }
 int main()
 {
-    int a[10];
-    Student s[10];
+    vector<Student> a;
     for (int i = 0; i < 10; i++)
     {
-        s[i] = Student("A", i, i + 1);
+        a.push_back(Student("A", 10 - i, i + 1));
     }
     for (int i = 0; i < 10; i++)
     {
-        s[i].print_info();
+        a[i].print_info();
     }
+    cout << "After Soring" << endl;
+    // sort(a.begin(), a.end(), comp);
+    sort(a.begin(), a.end());
+    for (int i = 0; i < 10; i++)
+    {
+        a[i].print_info();
+    }
+    // int a[10];
+    // Student s[10];
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     s[i] = Student("A", i, i + 1);
+    // }
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     s[i].print_info();
+    // }
     return 0;
 }
