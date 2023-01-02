8448db90369785e2428a7dffff52a29d5497d343
Abdullah Al Faroque <rfnshare@gmail.com>
2023-01-02 19:23:10 +0600

    variable complexity big o(n^2)

---
+++ src/phitron/C++/Week 2(Time Complicity And Array)/Lecture/order_n_square_complexity.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+variable complexity big o(n^2)
+*/
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        string ans = "NO\n";
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            if (a[i] == a[j])
+            {
+                ans = "YES\n";
+            }
+        }
+        cout << "i = " << i << " " << ans;
+    }
+
+    return 0;
+}
