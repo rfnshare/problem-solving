1fc47bf7f34069c056b2f51655d3c31a52796e14
Abdullah Al Faroque <rfnshare@gmail.com>
2023-01-02 20:00:36 +0600

    Time complexity o((n^2/2)) = o(n^2) Memory complexity big o(n^2)

---
+++ src/phitron/C++/Week 2(Time Complicity And Array)/Lecture/order_n_k_complexity.cpp
@@ -0,0 +1,36 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Time complexity o((n^2/2)) = o(n^2)
+Memory complexity big o(n^2)
+*/
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        string ans = "NO\n";
+        for (int j = i+1; j < n; j++)
+        {
+            if (i == j)
+            {
+                continue;
+            }
+            if (a[i] == a[j])
+            {
+                ans = "YES\n";
+            }
+        }
+        cout << "i = " << i << " " << ans;
+    }
+
+    return 0;
+}
