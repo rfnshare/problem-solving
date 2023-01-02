8cf878da5b2e1e6a7dad621ff38cd564b079dcba
Abdullah Al Faroque <rfnshare@gmail.com>
2023-01-02 12:33:16 +0600

    constant complexity big o(1)

---
+++ src/phitron/C++/Week 2(Time Complicity And Array)/Lecture/order_one_complicity.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+/*
+Time Complexity O(1)
+Space Complexity O(1)
+constant complexity big o(1)
+*/
+int main()
+{
+    int a, b, c;
+    cin >> a >> b >> c;
+    int maxi = max({a, b, c});
+    int mini = min({a, b, c});
+    int sum = a + b + c;
+    int mul = a * b * c;
+    cout << maxi << endl;
+    cout << mini << endl;
+    cout << sum << endl;
+    cout << mul << endl;
+    return 0;
+}
