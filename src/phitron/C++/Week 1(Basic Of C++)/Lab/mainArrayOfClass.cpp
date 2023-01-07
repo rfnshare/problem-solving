b02ade18a62086da6bef2c6e9c6a2a8238e70b7f
aalfaroque <rfnshare@gmail.com>
2023-01-07 21:55:09 +0600

    class array objects sorting using sort function

--- src/phitron/C++/Week 1(Basic Of C++)/Lab/mainArrayOfClass.cpp
+++ src/phitron/C++/Week 1(Basic Of C++)/Lab/mainArrayOfClass.cpp
@@ -30,17 +30,34 @@ public:
     }
     ~Student()
     {
-        cout << "Calling Destructor\n";
-        print_info();
+        // cout << "Calling Destructor\n";
+        // print_info();
+    }
+    bool operator<(Student s)
+    {
+        return age < s.age;
     }
 };
+// bool comp(Student a, Student b)
+// {
+//     //
+//     return a.id < b.id;
+// }
 int main()
 {
     vector<Student> s(10);
     for (int i = 0; i < 10; i++)
     {
-        s[i] = Student("A", i, 10 + i, "B", "C");
+        s[i] = Student("A", i, 30 - i, "B", "C");
     }
+    s.push_back(Student("Abdullah Al Faroque", 1530, 26, "Delwer Hossain", "Farida Akter"));
+    for (int i = 0; i < s.size(); i++)
+    {
+        s[i].print_info();
+    }
+    cout << "After Sorting...." << endl;
+    // sort(s.begin(), s.end(), comp);
+    sort(s.begin(), s.end());
     for (int i = 0; i < s.size(); i++)
     {
         s[i].print_info();
