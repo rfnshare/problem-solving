c5a95e6aac8dbc6ef82f8d7e988fdf45ff2910c3
aalfaroque <rfnshare@gmail.com>
2023-01-08 17:17:57 +0600

    problem 4

---
+++ src/phitron/C++/Week 2(Time Complicity And Array)/Practice/problem4.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int x, y;
+
+    cin >> x;
+    vector<int> a(x);
+    for (int i = 0; i < x; i++)
+    {
+        cin >> a[i];
+    }
+    cin >> y;
+    vector<int> b(y);
+    for (int i = 0; i < y; i++)
+    {
+        cin >> b[i];
+    }
+    for (int i = 0; i < x; i++)
+    {
+        for (int j = 0; j < y; j++)
+        {
+            // cout << a[i] << " ";
+            // cout << b[j] << " ";
+            // cout << "\n";
+            if (a[i] == b[j])
+            {
+                cout << a[i] << " ";
+            }
+        }
+    }
+}
+/*
+4
+1 2 3 5
+5
+1 6 2 7 3
+*/
