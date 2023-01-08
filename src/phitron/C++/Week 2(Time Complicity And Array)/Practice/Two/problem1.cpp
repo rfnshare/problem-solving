070fb5e5a34b581cf7fdbc92bd10a6b2b7b76c04
aalfaroque <rfnshare@gmail.com>
2023-01-08 23:38:18 +0600

    problem 1

---
+++ src/phitron/C++/Week 2(Time Complicity And Array)/Practice/Two/problem1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    /*
+    1. 
+    Time -> O(n)
+    Memory -> O(1)
+    2. 
+    Time -> O(n)
+    Memory -> O(1)
+    3. 
+    Time -> O(n)
+    Memory -> O(1)
+    4. 
+    Time -> O(n) or O(n^2/2)
+    Memory -> O(1)
+    5. 
+    Time -> O(n^2)
+    Memory -> O(1)
+    1. 
+    Time -> O(n^2)
+    Memory -> O(1)
+    */
+    return 0;
+}
