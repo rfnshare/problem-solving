973d2125c0dceae12a9cf0c431a7afaa60609638
aalfaroque <rfnshare@gmail.com>
2023-01-08 16:40:48 +0600

    Linear search & analysis complexity

--- src/phitron/C++/Week 2(Time Complicity And Array)/Lecture/Searching/linear search.cpp
+++ src/phitron/C++/Week 2(Time Complicity And Array)/Lecture/Searching/linear search.cpp
@@ -7,20 +7,24 @@ Looking Form Start To End
 */
 int main()
 {
-    int n,k;
-    cin>>n>>k;
-    vector<int>a(n);
-    for(int i=0;i<n;i++)
+    int n, k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for (int i = 0; i < n; i++)
     {
-        cin>>a[i];
+        cin >> a[i];
     }
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        if(a[i]==k)
+        if (a[i] == k)
         {
-            cout<<"YES"<<endl;
-            return 0;
+            cout << "YES->" << i << endl;
+        
         }
     }
     return 0;
 }
+/*
+7 5
+2 4 5 7 8 9 1
+*/
