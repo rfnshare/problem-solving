ad8275d77fc615068c6dc90408c39b31e2604037
aalfaroque <rfnshare@gmail.com>
2023-01-11 11:20:14 +0600

    Array Memory Concept

---
+++ src/phitron/C++/Week 3(Sorting & Linked List)/Lecture/LinkedList.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a[5] = {1, 2, 3, 4, 5};
+    cout << a << "\n";
+
+    cout << (a + 1) << "\n";
+    cout << (a + 2) << "\n";
+    cout << (a + 3) << "\n";
+    cout << (a + 4) << "\n";
+    cout << (a + 88) << "\n";
+    cout << a[88] << "\n";
+    return 0;
+}
